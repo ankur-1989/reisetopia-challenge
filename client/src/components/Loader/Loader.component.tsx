07443dcb26f1a263448b16bb889f9703e4e5d380
Ankur Gupta <ankur1989.23@gmail.com>
2022-05-11 20:14:23 +0200

    added loader component and wrapped the provider with store.

---
+++ client/src/components/Loader/Loader.component.tsx
@@ -0,0 +1,8 @@
+import "./loader.component.css";
+export default function Loader() {
+  return (
+    <div className="loader">
+      <h2 className="content">Loading data......</h2>
+    </div>
+  );
+}
